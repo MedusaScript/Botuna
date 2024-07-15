0d82ae20cba00410e29bd190a2b951928754e08b
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-15 23:33:22 +0700

    Update galEi.lua

--- galEi.lua
+++ galEi.lua
@@ -1,173 +1,86 @@
---[[
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⡶⠾⠶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠏⠀⠀⠀⠈⢿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡟⠀⠀⠀⠀⠀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⢸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡶⠶⠶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⠀⢠⣿⠁⠀⠀⠀⠀⠀⠀⠀⣠⡾⠛⠛⠛⢷⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡿⠋⠉⠙⢻⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠈⢻⡆⠀⠀⠀⣀⣠⣤⣤⣶⠶⠶⠿⠟⠀⠀⠀⠀⠀⡼⣷⣤⣤⣀⡀⠀⠀⠀⣴⠟⠁⠀⠀⠀⠀⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡀⠀⠀⠀⠀⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⢸⣇⣴⡾⠟⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠻⢶⣾⠋⠀⠀⠀⠀⠀⣰⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣇⠀⠀⠀⠀⠸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⢸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⡀⠀⠀⠀⠀⣿⡀⠀⠀⣀⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⢸⣿⡄⠀⠀⠀⠀⣀⣠⣤⣤⡶⠀⠀⠀⠀⠀⣴⣶⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣧⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡾⠛⠛⠻⣦⡀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡇⠀⠀⠀⠀⢸⣧⢀⣾⠋⠉⠈⠉⢻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡇⠀⠀⠀⠀⢸⣿⠻⠷⠶⠾⠟⠋⠉⢁⣿⠃⠀⠀⠀⠀⢰⡟⠀⠀⠈⠉⣻⡿⠂⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢷⣄⠀⠀⠀⠀⠀⢀⣴⡿⠋⠀⠀⠀⠀⢸⣷⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣶⣦⣄⠀⠀⠘⣿⠀⠀⠀⠀⠀⣿⣸⣇⠀⠀⠀⠀⠀⠹⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⣼⠏⠀⠀⠀⠀⢀⣿⠃⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⢀⣄⠀⠀⠀⠀⠀⠀⠹⣷⡄⠀⢀⣴⡿⠋⠀⠀⠀⠀⠀⢀⣼⠏⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⠁⠀⠀⠙⢷⣄⠀⢻⡇⠀⠀⠀⠀⢹⣿⣿⡀⠀⠀⠀⠀⠀⠘⢿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠠⣿⠀⠀⠀⠀⠀⠀⢰⣿⠀⠀⠀⠀⠀⣼⡇⠀⠀⢀⣾⠋⠀⠀⠀⠀⠀⣠⡿⠛⢷⣄⡀⠀⠀⠀⠀⠈⣿⣴⡿⠉⠀⠀⠀⠀⠀⢀⣴⣟⣁⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣇⠀⠀⠀⠀⠀⠙⢷⡜⣿⠀⠀⠀⠀⠀⣿⣹⣇⠀⠀⠀⠀⠀⠀⠈⢻⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠘⣿⣀⠀⠀⠀⣰⡟⠀⠀⠀⢸⣇⠀⠀⠀⠀⢀⣼⠟⠀⠀⠀⠙⢷⣄⠀⠀⠀⣠⡿⣿⡇⠀⠀⠀⠀⢀⣴⠿⠛⠛⠛⢷⣆⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣷⡀⠀⠀⠀⠀⢸⡗⢿⡆⠀⠀⠀⠀⣹⡏⣿⡀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠈⠛⢷⣶⣿⣏⣀⣀⣀⣀⡈⢻⣦⣄⣀⣤⡾⠋⠀⠀⠀⠀⠀⠀⠙⠿⠶⠿⠛⠁⠘⢷⣦⣀⣠⣴⣿⠁⠀⠀⠀⠀⠀⣿⡆⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣦⣤⣠⣴⠿⠁⠘⢿⣤⣀⣀⣴⡟⠁⢹⣇⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣗⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⣀⣤⡶⠿⠛⠋⠉⠉⠉⠉⠉⠙⠛⠿⢾⣭⡉⠉⠀⠀⠀⠀⠀⠀⣀⣤⣴⡶⠶⠾⠿⠷⠷⠶⢿⣭⣍⣁⢿⣆⡀⠀⠀⢀⣴⡿⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣽⡿⠿⠿⠛⠿⠿⠿⣿⣭⣅⡀⠀⠈⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣯⠀⠀⠀⠀⠀⣿⠀⠀⠀⣠⡾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢷⣄⠀⢀⣠⡶⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠻⣿⣿⠿⠛⠛⠉⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡶⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢷⣤⣽⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣷⡄⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⣿⡀⣠⣾⠋⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷⡿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣦⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⡀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠙⢿⣆⠀⠀⠀⠀⠀⠀⠸⣿⠀⠀⠀⠀⠀⣿⣷⡟⠁⠀⠀⠀⠀⠀⣠⣴⣿⣽⣶⣶⣬⣭⣙⣢⠄⠀⠀⠀⠀⠀⠈⡇⢧⡀⠀⢀⠀⠀⠀⠀⠀⣼⣉⡍⣍⣒⠤⣀⠀⠀⠀⠀⠀⠙⢷⣄⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠋⠀⠀⠀⠀⠀⠀⣀⠤⡔⣒⢒⣒⣲⣤⣀⡀⠀⠀⠀⠀⠀⠹⡇⠀⠀⠀⠀⠈⡧⡀⠀⠀⠀⠀⠈⢿⣧⡀⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⣿⡟⠀⠀⠀⠀⠀⢠⣾⠿⠋⠉⢀⣠⣴⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⣠⣿⣦⣾⣉⡞⠀⠀⠀⠀⠀⣿⠛⠻⣷⣮⡙⢶⡙⣤⠀⠀⠀⠀⠈⢻⣆⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⠁⠀⠀⠀⠀⢀⡔⢫⠒⣍⣶⡿⠿⠛⠋⠉⠙⠻⣦⡀⠀⠀⠀⠀⢻⡀⠀⠀⠀⠀⢳⢱⡄⠀⠀⠀⠀⠀⠻⣷⡄⠀⠀⠀⠠⣿⠀⠀⠀⠀⠀⣹⠃⠀⠀⠀⠀⢠⡿⢁⣠⣴⠾⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡾⠟⠁⣽⡏⢴⡇⠀⠀⠀⠀⢸⡿⠀⠀⠀⠙⢿⣇⡛⣎⠵⡀⠀⠀⠀⠀⢿⡆⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣯⠀⠀⠀⠀⢀⣎⡜⣡⢻⡿⠋⠀⠀⠀⠀⠀⠀⠀⣿⠇⠀⠀⠀⠀⢸⠃⠀⠀⠀⠀⠘⡇⣿⣆⠀⠀⠀⠀⠀⠙⣿⣆⠀⠀⠀⣿⠀⠀⠀⠀⠀⢼⠀⠀⠀⠀⠀⣿⡿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⠿⣋⣡⣤⣤⣤⣿⡏⢼⠀⠀⠀⠀⠀⣸⡇⠀⠀⠀⠀⠈⢻⣷⢨⠧⣹⠀⠀⠀⠀⠈⣿⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣷⣤⣠⣤⣾⣌⣹⣴⣿⠷⢶⣤⣤⣤⣀⠀⠀⣼⠏⠀⠀⠀⠀⠀⡎⢽⡀⠀⠀⠀⠀⢱⣸⣿⣦⠀⠀⠀⠀⠀⠈⢿⣧⡀⠀⣿⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⠾⠛⠉⣠⣾⢿⠋⠁⠀⠈⢻⡜⣺⠀⠀⠀⠀⠀⣿⠃⠀⠀⠀⠀⠀⠈⣿⡇⡎⢥⣇⠀⠀⠀⠀⣿⡆
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⢙⣻⡿⠋⠁⠀⠀⠈⣏⡙⣻⣷⣾⠏⠀⠀⠀⠀⠀⡼⡘⣾⡇⠀⠀⠀⠀⠘⡆⣿⡟⣷⡀⠀⠀⠀⠀⠀⠻⣷⡄⣿⡄⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡶⠟⠋⠁⠀⠀⢠⣿⢓⠏⠀⠀⠀⠀⢈⠖⡇⠀⠀⠀⠀⢠⣿⠀⠀⠀⠀⠀⠀⠀⣿⡇⢏⡲⣽⠀⠀⠀⠀⣽⡇
-⠀⠀⠀⠀⢀⣤⣤⣤⣄⣀⣤⣄⣀⢀⣴⡿⠋⠀⠀⠀⠀⠀⠀⡞⡜⢣⣿⠏⠀⠀⠀⠀⠀⡼⢰⣱⡿⣿⡀⠀⠀⠀⠀⢣⢹⣿⠈⢿⣄⠀⠀⠀⠀⠀⠙⣿⣿⡇⠀⠀⠀⠀⢸⣧⠀⠀⠀⠀⠀⠀⣤⣶⠟⠋⠁⠀⠀⠀⠀⠀⣠⣾⣯⠋⠀⠀⠀⠀⠀⡞⣸⠃⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⡏⢲⢙⡇⠀⠀⠀⠀⣿⠃
-⠀⠀⠀⣰⡿⠉⠀⠀⠙⠻⡭⣙⢻⠟⠁⠀⠀⠀⠀⠀⢀⡤⢚⢌⣷⣿⠃⠀⠀⠀⠀⢀⡜⠴⣹⡟⠁⢻⡇⠀⠀⠀⠀⠘⡇⣿⡆⠀⠻⣦⠀⠀⠀⠀⠀⠘⢿⡇⠀⠀⠀⠀⢸⣿⣧⠀⠀⠀⠀⠀⠈⠙⠷⣦⣤⣀⣀⣤⣴⣾⠿⠋⠀⠀⠀⠀⠀⠀⣼⡇⢼⠀⠀⠀⠀⠀⣾⡇⠀⠀⠀⠀⠀⠀⣼⡟⣌⠳⡞⠀⠀⠀⠀⢰⡟⠀
-⠀⠀⠀⢿⡇⠀⠀⠀⠀⠀⠈⠛⠁⠀⠀⠀⠀⠀⢀⡔⢫⢔⣯⣿⡿⠁⠀⠀⠀⠀⢀⠎⡜⣾⡟⠀⠀⠘⣿⠀⠀⠀⠀⠀⢳⢸⣿⠀⠀⠙⣷⡀⠀⠀⠀⠀⠘⠃⠀⠀⠀⠀⢸⣿⠹⣷⣄⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⠜⡼⠀⠀⠀⠀⠀⣿⠁⠀⠀⠀⠀⢀⣾⡟⡱⢌⠟⠀⠀⠀⠀⢀⣿⠃⠀
-⠀⠀⠀⠈⢿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢴⡝⣬⣷⠟⢫⡿⠁⠀⠀⠀⠀⢠⢋⣜⣾⠏⢀⣀⣄⣤⣿⡇⠀⠀⠀⠀⠘⡇⣿⡆⠀⠀⠘⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠈⠻⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡾⠛⣽⡏⡜⡇⠀⠀⠀⠀⢸⡿⠀⢀⣀⣤⣾⢟⡋⣴⠕⠁⠀⠀⠀⠀⣠⡿⠃⠀⠀
-⠀⠀⠀⠀⢀⣼⠿⠃⠀⠀⠀⠀⠀⠀⠀⠈⠳⢍⡻⡙⠿⣶⡿⠁⠀⠀⠀⠀⢠⢋⢖⣾⣿⠾⠛⠋⠉⠉⠫⣽⠀⠀⠀⠀⠀⢣⢹⣷⠀⠀⠀⠀⢻⣦⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠙⠛⢷⣦⣤⣀⣀⣀⣀⣀⣀⣀⣤⣴⣾⠛⠉⠀⠀⣿⡗⣸⠀⠀⠀⠀⠀⠘⠛⠿⠿⠿⠭⠒⠚⠉⠀⠀⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀
-⠀⠀⣠⣶⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⡝⡮⡽⠁⠀⠀⠀⠀⣠⡧⠟⠚⠉⠀⠀⠀⠀⠀⠀⠀⡇⡇⠀⠀⠀⠀⠘⡇⣿⡆⠀⠀⠀⠀⠹⣷⡀⠀⠀⠀⠀⠀⠀⢈⣿⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠛⣹⣿⢟⡿⠟⠿⢶⣴⣿⠱⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⠟⠁⠀⠀⠀⠀⠀
-⢠⡾⠛⠁⠀⠀⠀⠀⠀⢀⢔⣋⣳⣄⠀⠀⠀⠀⢀⡏⡶⠁⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣷⣿⠀⠀⠀⠀⠀⢳⢸⣷⠀⠀⠀⠀⠀⠘⢿⣄⠀⠀⠀⠀⠀⢀⣿⢀⣀⣤⣤⣴⣶⣶⣶⣦⣤⣤⣀⡀⠀⢠⣿⣃⡎⠀⠀⠀⠀⠉⠻⣥⡇⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀⣀⣠⣤⡶⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀
-⣿⠁⠀⠀⠀⠀⠀⣠⠔⣃⢮⣶⠟⠛⠿⣦⣤⣴⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣴⡾⠟⠛⠉⠀⢻⡇⠀⠀⠀⠀⠘⡎⣿⡆⠀⠀⠀⠀⠀⠈⠻⣦⣀⡀⠀⣀⣼⣿⡿⠿⠛⠋⠉⠉⠉⠉⠉⠉⠉⠛⠻⢷⣾⡿⣸⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⢸⡟⢛⣛⣿⣟⡛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠻⣧⣀⠀⢀⣠⣾⣷⣾⣾⣿⡿⠿⠿⢶⣄⠀⢸⡏⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⡶⠿⠛⠉⠁⠀⠀⠀⠀⠀⠸⣿⠀⠀⠀⠀⠀⢳⢸⣷⠀⠀⠀⠀⠀⠀⠀⣈⣽⣿⡿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡗⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⢟⡛⣋⡿⠿⠷⠶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠈⠛⠛⠛⢋⣠⣶⠿⠋⠁⠀⠀⠀⠀⢹⡇⠸⣷⡀⠀⠀⢀⣀⣤⣶⠾⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡇⠀⠀⠀⠀⠘⡍⣿⡆⠀⠀⣀⣤⣶⣿⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣤⣤⣄⣀⠀⠀⠀⢰⢡⠇⠀⠀⠀⠀⢸⣦⣀⠀⠀⠀⠀⠀⢀⡿⡑⢪⠜⠁⠀⠀⠀⠀⠀⠙⢷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⢀⣴⠾⠛⠉⠀⠀⠀⠀⠀⠀⠀⢀⡼⠿⡶⢿⡿⣿⣿⣿⣿⣧⣤⣤⣤⣤⣤⣤⣄⣀⣀⣀⣀⣀⣀⣀⣀⣀⣸⣿⠀⠀⠀⠀⠀⢳⢼⣷⣾⣿⠿⠟⠉⠁⠀⠀⠀⠀⠀⠀⣠⣴⡶⠟⠋⠉⠁⠀⠀⠈⣉⣽⣿⣶⣦⣜⡞⠀⠀⠀⠀⠀⡟⡿⢿⣷⣦⣀⣀⣠⣾⡰⢍⡏⠀⠀⢀⣴⣲⣄⠀⠀⠈⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⢠⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠓⠚⠒⠒⠒⠦⠲⠴⠬⠦⠭⠭⠭⠭⢭⠽⣙⣯⣙⣍⣛⣙⣋⣛⣙⣋⣛⡆⠀⠀⠀⠀⠘⡎⡏⠉⠀⠀⠀⠀⠀⠀⠀⣀⣤⡶⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⣴⡟⠉⠀⠉⢻⣃⡇⠀⠀⠀⠀⢰⢣⠃⠀⠉⠻⣿⣍⠉⢿⣷⣈⡇⠀⠀⠸⢽⣻⠟⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠘⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⣱⠀⠀⠀⠀⠀⢳⢳⠀⠀⠀⠀⣀⣠⣴⠾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⠀⠀⠀⠀⢸⢸⠁⠀⠀⠀⠀⣹⡾⠀⠀⠀⠀⠈⠙⢷⣤⡻⣷⣾⡄⠀⠀⠀⠀⠀⠀⢀⣼⠟⢀⣀⣀⠀⠀⠀⠀⠀⠀
-⠀⠀⠘⠿⣦⡀⠀⠀⠀⠀⠀⠀⠀⢤⣄⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣷⡇⠀⠀⠀⠀⠘⡜⣦⣤⡶⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡇⠀⠀⠀⠀⣎⡏⠀⠀⠀⠀⠀⣿⠿⣦⣀⠀⠀⠀⠀⠀⠉⠻⣦⣉⠻⢷⣤⣤⣤⣤⣴⠟⣫⡾⠛⠉⠛⢻⣦⠀⠀⠀⠀
-⠀⠀⠀⠀⠈⠻⣷⣄⠀⠀⠀⠀⠀⠀⠈⠻⣿⡛⠛⢛⣿⡿⠿⠿⠿⣿⣿⠿⠿⠷⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠿⣷⠀⠀⠀⠀⠀⢳⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡀⠀⠀⠀⢰⢡⠇⠀⠀⠀⠀⢸⡟⠀⠈⠛⢷⣤⡀⠀⠀⠀⠀⠈⠛⢷⣄⠀⠉⠉⠀⣠⡾⠋⠀⠀⠀⠀⠀⣿⡆⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠙⢷⣤⡀⠀⠀⠀⠀⠀⠈⢿⣾⠟⠁⠀⠀⠀⠀⠀⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣄⠀⠀⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣤⣤⣴⡟⡼⠀⠀⠀⠀⠀⣾⠇⠀⠀⠀⠀⠙⠻⣦⣄⠀⠀⠀⠀⠀⠙⢿⣦⣠⡾⠋⠀⠀⠀⠀⠀⢀⣴⡟⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣦⣄⠀⠀⠀⢀⣾⡏⡀⠀⢀⣴⣶⡄⠀⠀⢸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢷⣦⣴⠾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⣿⣃⡇⠀⠀⠀⠀⢠⣿⠀⠀⠀⠀⠀⠀⠀⠈⠛⢷⣄⠀⠀⠀⠀⠀⠈⠏⠀⠀⠀⠀⠀⠀⣴⡿⠋⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠷⢶⠶⠟⢻⣧⠀⠀⠀⠛⠛⠁⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⠁⠀⠀⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡿⠋⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣧⣀⠀⠀⠀⠀⢀⣴⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣄⠀⠀⠀⣠⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡿⠟⠀⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⠶⠶⠾⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠷⠶⠾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠟⠁⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣦⣤⣤⡾⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-]]
-__f = {
-    ['__game'] = function()
-        local p = game.PlaceId
-        if p == 2753915549 then return "BloxFruits"
-        elseif p == 4442272183 then return "BloxFruits"
-        elseif p == 7449423635 then return "BloxFruits"
-        elseif p == 537413528 then return "BuildABoat"
-        elseif p == 16389398622 then return "DustyTrip"
-        elseif p == 286090429 then return "Arsenal"
-        else return "BladeBall" end
-    end;
-    ['__script'] = function(m) return "https://raw.githubusercontent.com/ZoiIntra/__Script/main/__Alchemy/__"..m..".lua" end;
-    ['__load'] = function(s) loadstring(game:HttpGet(s))() end;
-    ['__ismobile'] = function()
-        local uis = game:GetService("UserInputService")
-        if uis.TouchEnabled and not uis.KeyboardEnabled and not uis.MouseEnabled then return true
-        elseif not uis.TouchEnabled and uis.KeyboardEnabled and uis.MouseEnabled then return false end
-    end;
-    ['__executor'] = tostring(identifyexecutor())
-}
-local isFluxus = function()
-    local exec = string.lower(__f['__executor'])
-    if exec == "fluxus android" or string.find(exec, "flux") then
-        return true
-    else
-        return false
-    end
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))() --This Will Load The Script Code
+local Player = game.Players.LocalPlayer --This Will Reveal The Player Name
+  local Window = OrionLib:MakeWindow({
+		Name = "Medusa Keysystem",
+		HidePremium = false,
+		SaveConfig = true,
+		ConfigFolder = "OrionTest",
+        IntroText = "Loading Keysystem"       
+}) --This Will Load The Script Hub
+
+function MakeScriptHub()
+         loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/limitless-medusa/main/BrutalityV1'))() --Put The Script That Will Load If The Key Is Correct Here
 end
-if _G.run_time then game:GetService("Players").LocalPlayer:Kick("[ Error : 429 ] Too Many Execute") end
-if _G.StreamerMode then
-    pcall(function()
-        local a_protect = game:GetDescendants()
-        local n_protect =  game:GetService("Players").LocalPlayer.DisplayName
-        local f_protect =  game:GetService("Players").LocalPlayer.Name
-        for i=1,#a_protect do
-            if a_protect[i].ClassName == "TextLabel" then
-                if a_protect[i].Text == n_protect then
-                    a_protect[i].Text = "Protect By Alchemy"
-                elseif a_protect[i].Text == "@"..f_protect then
-                    a_protect[i].Text = "@Protect By Alchemy"
-                end
-            end
+
+OrionLib:MakeNotification({
+	Name = "Logged In!",
+	Content = "You need key "..Player.Name..".",
+	Image = "rbxassetid://16058297648",
+	Time = 5
+}) --Notification
+
+getgenv().Key = "Tom-New3rranebe" --Put The Correct Key Here
+getgenv().KeyInput = "string" --Require For The Key To Work
+
+local Tab = Window:MakeTab({
+	Name = "Key",
+	Icon = "rbxassetid://16058297648",
+	PremiumOnly = false
+}) --Making A Tab
+
+Tab:AddTextbox({
+	Name = "Key",
+	Default = "Enter Key.",
+	TextDisappear = true,
+	Callback = function(Value)
+		getgenv().KeyInput = Value
+	end	  
+}) --You Will Enter The Key Here
+
+Tab:AddButton({
+    Name = "Check Key",
+    Callback = function()
+        if getgenv().KeyInput == getgenv().Key then
+            OrionLib:MakeNotification({
+                Name = "Checking Key",
+                Content = "Checking The Key You Entered",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
+            wait(2)
+            OrionLib:MakeNotification({
+                Name = "Correct Key!",
+                Content = "The key you entered is Correct.",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
+            wait(1)
+            OrionLib:Destroy()
+            wait(.3)
+            MakeScriptHub()
+        else
+           OrionLib:MakeNotification({
+                Name = "Checking Key",
+                Content = "Checking The Key You Entered",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
+            wait(2)
+            OrionLib:MakeNotification({
+                Name = "Incorrect Key!",
+                Content = "The key you entered is incorrect.",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
         end
-    end)
-end
-local loadFluxusWarn = function()
-    local warnING = Instance.new("ScreenGui")
-    local MainFrame = Instance.new("Frame")
-    local MainCorner = Instance.new("UICorner")
-    local ImageLabel = Instance.new("ImageLabel")
-    local Submit = Instance.new("TextButton")
-    local SubmitCorner = Instance.new("UICorner")
-    local Information = Instance.new("TextLabel")
-    local Header = Instance.new("TextLabel")
-    warnING.Name = "warnING"
-    warnING.Parent = game:WaitForChild("CoreGui")
-    warnING.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    MainFrame.Name = "MainFrame"
-    MainFrame.Parent = warnING
-    MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-    MainFrame.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-    MainFrame.Position = UDim2.new(0.499723464, 0, 0.499925047, 0)
-    MainFrame.Size = UDim2.new(0, 0, 0, 0)
-    MainFrame.ZIndex = 0
-    MainCorner.Name = "MainCorner"
-    MainCorner.Parent = MainFrame
-    ImageLabel.Parent = MainFrame
-    ImageLabel.AnchorPoint = Vector2.new(0.5, 0.5)
-    ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    ImageLabel.BackgroundTransparency = 1.000
-    ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    ImageLabel.BorderSizePixel = 0
-    ImageLabel.Position = UDim2.new(0.498768479, 0, 0.493386239, 0)
-    ImageLabel.Size = UDim2.new(0, 188, 0, 188)
-    ImageLabel.Image = "rbxassetid://696390628"
-    ImageLabel.ImageTransparency = 0.900
-    Submit.Name = "Submit"
-    Submit.Parent = MainFrame
-    Submit.AnchorPoint = Vector2.new(0.5, 0.5)
-    Submit.BackgroundColor3 = Color3.fromRGB(152, 152, 152)
-    Submit.BackgroundTransparency = 0.800
-    Submit.Position = UDim2.new(0.498482376, 0, 0.773373246, 0)
-    Submit.Size = UDim2.new(0, 355, 0, 30)
-    Submit.Font = Enum.Font.GothamBold
-    Submit.Text = "I understand but I want to continue using Fluxus."
-    Submit.TextColor3 = Color3.fromRGB(255, 255, 255)
-    Submit.TextSize = 14.000
-    SubmitCorner.CornerRadius = UDim.new(0, 5)
-    SubmitCorner.Name = "SubmitCorner"
-    SubmitCorner.Parent = Submit
-    Information.Name = "Information"
-    Information.Parent = MainFrame
-    Information.BackgroundColor3 = Color3.fromRGB(255, 254, 216)
-    Information.BackgroundTransparency = 1.000
-    Information.ClipsDescendants = true
-    Information.Position = UDim2.new(0.0834580585, 0, 0.340372086, 0)
-    Information.Size = UDim2.new(0, 336, 0, 60)
-    Information.Font = Enum.Font.GothamBold
-    Information.Text = "Hello!, we detected that your executor is Fluxus, we recommend using Codex, Arceus, Trigon or Cubix as they crash frequently."
-    Information.TextColor3 = Color3.fromRGB(255, 254, 216)
-    Information.TextSize = 16.000
-    Information.TextWrapped = true
-    Information.TextYAlignment = Enum.TextYAlignment.Top
-    Header.Name = "Header"
-    Header.Parent = MainFrame
-    Header.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Header.BackgroundTransparency = 1.000
-    Header.ClipsDescendants = true
-    Header.Position = UDim2.new(0.0295566507, 0, 0.128732026, 0)
-    Header.Size = UDim2.new(0, 381, 0, 39)
-    Header.Font = Enum.Font.GothamBold
-    Header.Text = "Executor Warning"
-    Header.TextColor3 = Color3.fromRGB(255, 255, 255)
-    Header.TextSize = 32.000
-    Submit.MouseButton1Click:Connect(function()
-        warnING.Enabled = false
-    end)
-    wait(); MainFrame:TweenSize(UDim2.new(0, 406, 0, 189), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, 0.3, true)
-end
-_G.run_time = true
-if isFluxus() then loadFluxusWarn() end
-if _G.Aimbot then __f['__load'](__f['__script']("Aimbot"))
-elseif _G.FruitFinder then __f['__load'](__f['__script']("FruitFinder"))
-elseif _G.Old then __f['__load'](__f['__script']("BloxFruitsOld"))
-else __f['__load'](__f['__script'](__f['__game']())) end
+    end
+}) --This Will Check The Key You Entered
+
+Tab:AddButton({
+	Name = "Copy Key",
+	Callback = function()
+      		setclipboard("https://pastefy.app/2DHgmGCV") --This Will Copy The Link Of The Key
+  	end    
+}) 
+    
+OrionLib:Init() --Require If The Script Is Done
