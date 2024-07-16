e7265723a678ac8113ed03969d24e69992bc3fe6
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-17 06:29:21 +0700

    Update galEi.lua

--- galEi.lua
+++ galEi.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("BRUTALITY HUB X MEDUSA SCRIPT - ALL SCRIPT IN ONE", "BloodTheme")
+local Window = Library.CreateLib("BRUTALITY HUB X MEDUSA SCRIPT - ALL SCRIPT IN ONE", "Ocean")
 
 --UTAMA
 local Main = Window:NewTab("PREMIUM")
