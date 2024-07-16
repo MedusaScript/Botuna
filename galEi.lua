77cd6be757c32d1faf9eefa4abde4effdf9625de
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-17 03:20:16 +0700

    Update galEi.lua

--- galEi.lua
+++ galEi.lua
@@ -3,15 +3,20 @@ local Window = Library.CreateLib("BRUTALITY HUB - Universal", "Midnight")
 
 --MAIN
 local Main = Window:NewTab("Main")
-local MainSection = Main:NewSection("Main")
+local MainSection = Main:NewSection("Script All Game")
+
+
+MainSection:NewButton("Brutality CHEST", "Blox Fruit", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/Botuna/main/Brutalitychest.lua'))()
+end)
 
 
 MainSection:NewButton("Infinity Yield", "Great command pannel", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
 
-MainSection:NewButton("Simple", "Great command pannel", function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/CCYKNJQK"))()
+MainSection:NewButton("Simple Fitur", "Hack Game", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/BrutalityV2/main/HackBf.lua'))()
 end)
 
 
@@ -20,6 +25,16 @@ MainSection:NewButton("Back/Front Flip", "Makes you do gymnatics", function()
 end)
 
 
-MainSection:NewButton("Brutality V1", "Blox Fruit", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MedusaScript/limitless-medusa/main/BrutalityV1"))()
+MainSection:NewButton("Brutality Hub V1", "Blox Fruit", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/limitless-medusa/main/BrutalityV1'))()
+end)
+
+
+MainSection:NewButton("Brutality Hub V2", "Blox Fruit", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/limitless-medusa/main/BrutalityV1'))()
+end)
+
+
+MainSection:NewButton("Brutality Hub V3", "Blox Fruit", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/limitless-medusa/main/BrutalityV1'))()
 end)
