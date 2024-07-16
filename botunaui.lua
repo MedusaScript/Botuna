68b51a8305f37eac71368d051334c9d926db10be
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-17 06:59:06 +0700

    Update botunaui.lua

--- botunaui.lua
+++ botunaui.lua
@@ -86,10 +86,10 @@ local themeStyles = {
     },
     Ocean = {
         SchemeColor = Color3.fromRGB(16, 156, 3),
-        Background = Color3.fromRGB(26, 32, 58),
-        Header = Color3.fromRGB(38, 45, 71),
+        Background = Color3.fromRGB(38, 45, 71),
+        Header = Color3.fromRGB(0, 0, 0),
         TextColor = Color3.fromRGB(255, 255, 255),
-        ElementColor = Color3.fromRGB(38, 45, 71)
+        ElementColor = Color3.fromRGB(0, 0, 0)
     },
     Midnight = {
         SchemeColor = Color3.fromRGB(26, 189, 158),
