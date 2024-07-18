3f7f6b55c3d36682387151cdd37a1ac88cd52562
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-18 20:02:17 +0700

    Update botunaui.lua

--- botunaui.lua
+++ botunaui.lua
@@ -1,12 +1,18 @@
 --made by sifix, for support .gg/qXxx5pSYA3
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
-local PhantomForcesWindow = Library:NewWindow("Brutality Hub")
+local PhantomForcesWindow = Library:NewWindow("Brutality Hub (beta)")
 
 local AutoParry = PhantomForcesWindow:NewSection("Main")
 
 AutoParry:CreateToggle("Parry+Visualizer", function()loadstring(game:HttpGet("https://scriptblox.com/raw/UPD-Blade-Ball-op-autoparry-with-visualizer-8652",true))()
 end) --done
+
+AutoParry:CreateToggle("Auto Pary V1", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/MrDemonSkrtSkrt/Auto-Parry/main/Blade%20Ball",true))()
+end)
+
+AutoParry:CreateToggle("Auto Pary V2", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/2VQuiet/AutoParryBladeBall/main/Auto%20Parry%20Script",true))()
+end)
 
 AutoParry:CreateToggle("Auto Clash", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/datshort1/datmodhub/main/AutoClash",true))()
 end)
@@ -14,7 +20,7 @@ end)
 AutoParry:CreateToggle("Auto Detect Spam", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/MC%3ABlade%20Ball%20Spam",true))()
 end)
 
-local AutoParry = PhantomForcesWindow:NewSection("Test")
+local AutoParry = PhantomForcesWindow:NewSection("Setting")
 
 AutoParry:CreateToggle("Detect Spam V2", function()loadstring(game:HttpGet("https://pastebin.com/raw/t2391h1A",true))()
 end)
