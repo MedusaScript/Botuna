31369c293ec35dd820f8853f72592e273c6faa8d
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-18 19:51:17 +0700

    Update botunaui.lua

--- botunaui.lua
+++ botunaui.lua
@@ -5,14 +5,16 @@ local PhantomForcesWindow = Library:NewWindow("Brutality Hub")
 
 local AutoParry = PhantomForcesWindow:NewSection("Main")
 
-AutoParry:CreateToggle("Auto Parry", function()loadstring(game:HttpGet("https://scriptblox.com/raw/UPD-Blade-Ball-op-autoparry-with-visualizer-8652",true))()
-end)
+AutoParry:CreateToggle("Parry+Visualizer", function()loadstring(game:HttpGet("https://scriptblox.com/raw/UPD-Blade-Ball-op-autoparry-with-visualizer-8652",true))()
+end) --done
 
 AutoParry:CreateToggle("Auto Clash", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/datshort1/datmodhub/main/AutoClash",true))()
 end)
 --Toggle baru
 AutoParry:CreateToggle("Auto Detect Spam", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/MC%3ABlade%20Ball%20Spam",true))()
 end)
+
+local AutoParry = PhantomForcesWindow:NewSection("Test")
 
 AutoParry:CreateToggle("Detect Spam V2", function()loadstring(game:HttpGet("https://pastebin.com/raw/t2391h1A",true))()
 end)
