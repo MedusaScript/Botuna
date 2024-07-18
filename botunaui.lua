6a9d90d73e37de1bbb38add58a5538df1ad4ff9a
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-18 20:13:10 +0700

    Update botunaui.lua

--- botunaui.lua
+++ botunaui.lua
@@ -14,6 +14,9 @@ end)
 AutoParry:CreateToggle("Auto Pary V2", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/2VQuiet/AutoParryBladeBall/main/Auto%20Parry%20Script",true))()
 end)
 
+AutoParry:CreateToggle("Auto Pary V3", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/2VQuiet/AutoParryBladeBall/main/Auto%20Parry%20Script",true))()
+end)
+
 AutoParry:CreateToggle("Auto Clash", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/datshort1/datmodhub/main/AutoClash",true))()
 end)
 --Toggle baru
@@ -22,8 +25,19 @@ end)
 
 local AutoParry = PhantomForcesWindow:NewSection("Setting")
 
+AutoParry:CreateToggle("Anti Lag", function()loadstring(game:HttpGet("https://pastebin.com/raw/1RfvPdwX"))()
+end)
+
+AutoParry:CreateToggle("Auto Detect Spam", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/MC%3ABlade%20Ball%20Spam",true))()
+end)
+
 AutoParry:CreateToggle("Detect Spam V2", function()loadstring(game:HttpGet("https://pastebin.com/raw/t2391h1A",true))()
 end)
 
 AutoParry:CreateToggle("Visualizer", function()loadstring(game:HttpGet("https://pastebin.com/raw/2RzCXHZH"))()
+end)
+
+local AutoParry = PhantomForcesWindow:NewSection("Shop")
+
+AutoParry:CreateToggle("Buy Sword Crate", function()loadstring(game:HttpGet("https://pastebin.com/raw/1RfvPdwX"))()
 end)
