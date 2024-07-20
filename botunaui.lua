ab52f9184ea5844b8f95ccb259154d9f7d62eb41
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-21 00:38:18 +0700

    Update botunaui.lua

--- botunaui.lua
+++ botunaui.lua
@@ -1,75 +1,126 @@
---// leaked by discord.gg/visualontop
-wait(1.5)
+local PandaAuth = loadstring(game:HttpGet("https://raw.githubusercontent.com/Panda-Repositories/PandaKS_Libraries/main/library/LuaLib/ROBLOX/VAL", true))()
 
-game.StarterGui:SetCore("SendNotification", {
+local InternalTable = { -- change
+    Service = "Brutality Hub", -- Identifier Name *
+    APIToken = "7JF0Mui0kZ7F4Dud", -- API Token (For Library Encryption) *
+    VigenereKey = tostring({}) .. tostring(function() end) .. tostring(workspace["GetServerTimeNow" .. ("\0"):rep(math.random(2, 32))](workspace) + math.random(os.clock(), 999999999 or os.time())) .. ("\0"):rep(math.random(2, 32) % os.time() + os.clock()),
+    TrueEndpoint = tostring({}) .. tostring(function() end) .. tostring(workspace["GetServerTimeNow" .. ("\0"):rep(math.random(2, 32))](workspace) + math.random(os.clock(), 999999999 or os.time())) .. ("\0"):rep(math.random(2, 32) % os.time() + os.clock()),
+    FalseEndpoint = tostring({}) .. tostring(function() end) .. tostring(workspace["GetServerTimeNow" .. ("\0"):rep(math.random(2, 32))](workspace) + math.random(os.clock(), 999999999 or os.time())) .. ("\0"):rep(math.random(2, 32) % os.time() + os.clock()),
+    Webhook = nil,
+    Debug = false 
+}
 
-Title = "Salam Dari BOTUNA"; -- teks notif atas
-
-Text = "Bocah Tua Nakal"; -- teks notif bawah
-
-Icon = "rbxassetid://18551656195"; -- logo notif
-Duration = 5; -- how long the notification should in secounds
-
+local Internal = setmetatable({}, {
+	__index = function(self, key)
+		return rawget(InternalTable, key)
+	end;
+	__newindex = function(self, key, value)
+		while true do end; repeat until false; print(debug.traceback()) return
+	end;
+	__tostring = function(self)
+		while true do end; repeat until false; print(debug.traceback()) return
+	end;
 })
--- batas notify
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
-local PhantomForcesWindow = Library:NewWindow("BRUTALITY (BETA)")
+local SetInternal = PandaAuth.SetInternal; PandaAuth:SetInternal(Internal)
 
-local MainKillingCheats = PhantomForcesWindow:NewSection("Main")
+local Type = "Key"
+local UI = "MaggixV2"
+local KeySys =
+    loadstring(
+    game:HttpGet(
+        "https://raw.githubusercontent.com/KrypDeveloper/EZ-KeySystem/main/V5/UIS/" .. Type .. "/" .. UI .. ".lua"
+    )
+)()
 
-MainKillingCheats:CreateToggle("Auto Parry", function(value)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/2VQuiet/AutoParryBladeBall/main/Auto%20Parry%20Script"))()
-end)
+local Validate = function(key)
+	local result = PandaAuth:ValidateKey(key)
+	
+	local Crypt = PandaAuth:GetInternal().Crypt
+	
+	local SHA256 = Crypt:SHA256(Internal.TrueEndpoint, Internal.VigenereKey, nil, nil)
+	
+	local SyncTrue = Crypt:EncryptC(SHA256, Internal.VigenereKey, nil)
+	
+	local IteratedTables = {}
+	
+	local ValueSpoofed; ValueSpoofed = function(val, tbl)
+		local ret = nil
+		
+		for i, v in pairs(tbl) do
+			if v == val then
+				print(i, v, val)
+				
+				ret = true 
+				
+				break
+			elseif type(tbl) == "table" and not IteratedTables[tbl] then
+				IteratedTables[tbl] = true
+				
+				ret = ValueSpoofed(val, tbl)
+				
+				break
+			end
+		end
+		
+		table.clear(IteratedTables)
+		
+		return ret
+	end
+	
+	if ValueSpoofed(result["KEY"], {result, SHA256, SyncTrue}) 
+	or ValueSpoofed(result["Encrypted"], {result}) then
+		while true do end do return end
+	end
+	
+	if getmetatable(getfenv(PandaAuth.SetInternal)["getrenv\0"])["__newindex"]() == SetInternal and PandaAuth.SetInternal ~= SetInternal and getmetatable(getfenv(select(2, PandaAuth:SetInternal(Internal)))["getrenv\0"])["__newindex"]() == SetInternal and PandaAuth:SetInternal(Internal) == SetInternal then
+		if result and result["KEY"] and type(result["ENV"]) == type(getfenv(1)) and result["Raw"] == Internal.TrueEndpoint and result["Encrypted"] == SyncTrue and type(result["Premium"]) == "boolean" and PandaAuth.Validated[1] == Internal.TrueEndpoint and PandaAuth.Validated[2] == true then
+			print("Key is valid:")
+			
+			print("Is key premium:", result["Premium"])
+			
+			return true
+		elseif result and result["Raw"] == Internal.FalseEndpoint and PandaAuth.Validated[1] == Internal.FalseEndpoint and PandaAuth.Validated[2] == false then
+			print("Key is invalid.")
+			
+			return false
+		else
+			while true do end do return end
+		end
+	else
+		while true do end do return end
+	end
+	
+	return true
+end
 
-MainKillingCheats:CreateToggle("Aura", function(value)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MedusaScript/BrutalityV2/main/aura-hentai.lua"))()
-end)
+if isfile("Pelinda Data/Infinix/Key.txt") and Validate(readfile("Pelinda Data/Infinix/Key.txt")) then
+    print("Validated!")
+else
+    local Window = KeySys:CreateGui(
+        {
+            Data = {
+                HubName = "Brutality Hub"
+            },
+            Discord = "https://discord.com/invite/ThHhY24N6T"
+        }
+    )
 
-MainKillingCheats:CreateToggle("AIM MECANISME", function(value)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/M%3ABlade%20Ball%20Mechanism"))()
-end)
+    Window:SetGetKey(
+        function()
+            setclipboard(PandaAuth:GetKey())
+        end
+    )
 
-MainKillingCheats:CreateToggle("MOD", function(value)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MedusaScript/BrutalityV2/main/aura-hentai.lua"))()
-end)
-
-MainKillingCheats:CreateToggle("Sword Crate", function(value)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MedusaScript/BrutalityV2/main/sword1.lua"))()
-end)
-
-MainKillingCheats:CreateToggle("Explosion Crate", function(value)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MedusaScript/BrutalityV2/main/Sword2.lua"))()
-end)
-
-local MiscKillingCheats = PhantomForcesWindow:NewSection("Setting")
-
-MiscKillingCheats:CreateButton("Anti Lag", function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/1RfvPdwX", true))()
-    print("TrilleD")
-end)
-
-MiscKillingCheats:CreateButton("Shaders", function()
-    loadstring(game:HttpGet(('https://pastefy.app/xXkUxA0P/raw'), true))()
-    print("TrilleD")
-end)
-
-MiscKillingCheats:CreateButton("Fps Booster", function()
-    loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Fps-booster-9504", true))()
-    print("TrilleD")
-end)
-
-local Credits = PhantomForcesWindow:NewSection("Credits")
-
-Credits:CreateButton("YT: @MedusaScriptRoblox", function()
-    print("TrilleD")
-end)
-
-Credits:CreateButton("CODE: by BOTUNA", function()
-    print("TrilleD")
-end)
-
-Credits:CreateButton("Join Discord", function()
-    setclipboard("https://discord.gg/medusa-script-1182005198206545941")
-    print("TrilleD")
-end)
+    Window:SetCheckKey(
+        function(key)
+            if Validate(key) then
+                print("Whitelisted")
+                
+                writefile("Pelinda Data/Infinix/Key.txt", key)
+                
+                Window:DestroyGui()
+            end
+        end
+    )
+end
