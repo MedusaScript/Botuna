f9782260d3b74aaef2525559549118aa32afe98b
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-07-31 20:15:09 +0700

    Update sa.lua

--- sa.lua
+++ sa.lua
@@ -1,1 +1,80 @@
+--https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua--Mentahan Ui
+--]]
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/MedusaScript/Botuna/main/UI-Universal-Medusa.lua"))()
+local Window = Library.CreateLib("BRUTALITY HUB X MEDUSA SCRIPT - ALL SCRIPT IN ONE", "Ocean")
 
+--UTAMA
+local Main = Window:NewTab("THE BEST")
+local MainSection = Main:NewSection("PRISON LIFE - No Key") --TANDA --
+
+MainSection:NewButton("Brutality Hub", "ARSENAL", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MedusaScript/Botuna/main/ArsenalV1.lua"))()
+end)
+
+local MainSection = Main:NewSection("Blox Fruit - No Key") --TANDA --
+
+MainSection:NewButton("Hack Blox Fruit", "Hack Game", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/BrutalityV2/main/HackBf.lua'))()
+end)
+
+MainSection:NewButton("Kaitun (beta)", "Blox Fruit", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/Botuna/main/KaitunV1.lua'))()
+end)
+
+MainSection:NewButton("FARM CHEST", "Blox Fruit", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/Botuna/main/Chest.lua'))()
+end)
+
+MainSection:NewButton("Brutality Hub V1", "Blox Fruit", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/limitless-medusa/main/BrutalityV1'))()
+end)
+
+MainSection:NewButton("Brutality Hub V2", "Blox Fruit", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/Botuna/main/BrutalityV2.lua'))()
+end)
+
+MainSection:NewButton("Brutality Hub V3", "Blox Fruit", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/MedusaScript/Botuna/main/BrutalityV3.lua'))()
+end)
+
+local Main = Window:NewTab("BLOX FRUIT")
+local MainSection = Main:NewSection("BLOX FRUIT - No Key") --TANDA --
+
+MainSection:NewButton("Redz Hub", "Blox Fruit", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
+end)
+
+MainSection:NewButton("Vector Hub", "Blox Fruit", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/VectorHub/main/uh.lua"))()
+end)
+
+MainSection:NewButton("Unknow Hub", "Blox Fruit", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BestScriptEverr/Main-/main/UnknownHubV3"))()
+end)
+
+MainSection:NewButton("Fai Fao Hub", "Blox Fruit", function()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/PNguyen0199/Script/main/Fai-Fao-Ver2.lua")()
+end)
+
+MainSection:NewButton("ScriptBlox Hub", "Blox Fruit", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Script-Blox/Script/main/Script-Blox'))()
+end)
+
+MainSection:NewButton("Ganteng Hub", "Blox Fruit", function()
+    loadstring(game:HttpGet("https://isnahamzahpastebin.tech/gantenghubv1_new"))()
+end)
+
+local Main = Window:NewTab("KING LEGACY")
+local MainSection = Main:NewSection("KING LEGACY - No Key") --TANDA --
+
+local Main = Window:NewTab("BLADE BALL")
+local MainSection = Main:NewSection("BLADE BALL - No Key") --TANDA --
+
+local Main = Window:NewTab("PRISON LIFE")
+local MainSection = Main:NewSection("PRISON LIFE - No Key") --TANDA --
+
+local Main = Window:NewTab("CDID")
+local MainSection = Main:NewSection("CDID - No Key") --TANDA --
+
+local Main = Window:NewTab("PETSIM")
+local MainSection = Main:NewSection("PETSIM - No Key") --TANDA --
