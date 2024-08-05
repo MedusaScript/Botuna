38bd96709212c6cfff154306d744f3d44fbb14d3
Medusa <155561619+MedusaScript@users.noreply.github.com>
2024-08-05 21:04:04 +0700

    Update sa11aa.lua

--- sa11aa.lua
+++ sa11aa.lua
@@ -2336,7 +2336,7 @@ do
         "DRAGONABUSE",
     }
 
-Tabs.Sh:AddButton({
+Tabs.Mt:AddButton({
         Title = "Redeem All Code",
         Description = "Get 2x exp and reset stats",
         Callback = function()            
@@ -3908,12 +3908,12 @@ end)
         
     Tabs.If:AddParagraph({
         Title = "Information",
-        Content = ""
+        Content = "click the button to copy automatically"
     })
 
     Tabs.If:AddButton({
         Title = "Join Discord",
-        Description = "Use bypass In Discord, Click for copy link",
+        Description = "Use bypass In Discord & Webhook Notify",
         Callback = function()
             setclipboard("https://discord.gg/brutalityhub")
         end
@@ -5322,6 +5322,37 @@ end)
             end
     end)
     
+    local Toggle = Tabs.Qs:AddToggle("MyToggle", {Title = "Infinite Jump", Default = false })
+    Toggle:OnChanged(function()
+        _G.infinjump = true
+ 
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
+Mouse.KeyDown:connect(function(k)
+if _G.infinjump then
+if k:byte() == 32 then
+Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+Humanoid:ChangeState("Jumping")
+wait(0.1)
+Humanoid:ChangeState("Seated")
+end
+end
+end)
+ 
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
+Mouse.KeyDown:connect(function(k)
+k = k:lower()
+if k == "r" then
+if _G.infinjump == true then
+_G.infinjump = false
+else
+_G.infinjump = true
+end
+end
+end)
+end)
+
     local Input = Tabs.Ms:AddInput("Input", {
         Title = "Job Id Webhook",
         Default = "",
