13c953bec864f979d50cf6b3e98f8b41768f11af
Rafael <155561619+MedusaScript@users.noreply.github.com>
2024-08-23 19:54:26 +0700

    Update and rename halahcapeNaruto777.lua to halahcapeNaruto7717.lua

--- halahcapeNaruto7717.lua
+++ halahcapeNaruto7717.lua
@@ -1108,10 +1108,55 @@ local D = Library:AddTab("Devil Fruit","7044233235")
 local Ss = Library:AddTab("MISC","11156061121")
 local D = Library:AddTab("Devil Fruit","7044233235")
 local Dms = Library:AddTab("Misc","11156061121")
-H:AddToggle("Auto Farm Level"function(value)
+
+local Time = H:AddLabel("Run Script");spawn(function() getgenv().Time = true;while true do wait(.1) UpdateTime() end end);function UpdateTime() local date = os.date("*t");local hour = (date.hour) % 24;local ampm = hour < 12 and "AM" or "PM";local timezone = string.format("%02i:%02i:%02i %s", ((hour -1) % 12) + 1, date.min, date.sec, ampm);local datetime = string.format("%02d/%02d/%04d", date.day, date.month, date.year);local LocalizationService = game:GetService("LocalizationService");local Players = game:GetService("Players");local player = Players.LocalPlayer;local name = player.Name;local result, code = pcall(function()   return LocalizationService:GetCountryRegionForPlayerAsync(player)  end);Time:Set(" : " .. timezone);Time:Set("Executor Time: " .. datetime .. " [ " .. code .. " ]");spawn(function() if getgenv().Time then pcall(function()  while wait() do  Time()  end end) end end) end
+
+local Time1 = H:AddLabel("Executor Time")
+
+function UpdateTime()
+local GameTime = math.floor(workspace.DistributedGameTime+0.5)
+local Hour = math.floor(GameTime/(60^2))%24
+local Minute = math.floor(GameTime/(60^1))%60
+local Second = math.floor(GameTime/(60^0))%60
+Time1:Set("[GameTime] : Hours : "..Hour.." Min : "..Minute.." Sec : "..Second)
+end
+
+spawn(function()
+while task.wait() do
+pcall(function()
+UpdateTime()
+end)
+end
 end)
 
-H:AddSeperator("Update Log")
+local Client = H:AddLabel("Client")
+
+function UpdateClient()
+local Fps = workspace:GetRealPhysicsFPS()
+Client:Set("[Fps] : "..Fps)
+end
+
+spawn(function()
+while true do wait(.1)
+UpdateClient()
+end
+end)
+
+local Client1 = H:AddLabel("Client")
+
+function UpdateClient1()
+local Ping = game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString()
+Client1:Set("[Ping] : "..Ping)
+end
+
+spawn(function()
+while true do wait(.1)
+UpdateClient1()
+end
+end)
+
+H:AddLabel("Work For PC/Mobile")
+H:AddLabel("Thanks For Using Brutality Hub")
 H:AddButton("Youtube Brutality Hub",function()
     setclipboard("https://youtube.com/@medusascriptroblox/")
     end)
