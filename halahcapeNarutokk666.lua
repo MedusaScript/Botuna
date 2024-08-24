6973ae5fde68e90ff7773873f9e0016970fa1626
Rafael <155561619+MedusaScript@users.noreply.github.com>
2024-08-24 20:48:35 +0700

    Update and rename halahcapeNarutokk676.lua to halahcapeNarutokk666.lua

--- halahcapeNarutokk666.lua
+++ halahcapeNarutokk666.lua
@@ -437,7 +437,7 @@ end
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vldtncywdlojtnvjlmvyrbszljd/48139u372988729j82hd/main/%3D2-e0dk2.lua"))()
 local Window = redzlib:MakeWindow({ Title = "BRUTALITY HUB : Meme Sea", SubTitle = "Made by Medusa Script", SaveFolder = "redzHub-MemeSea.json" })
 Window:AddMinimizeButton({
-  Button = { Image = "rbxassetid://15298567397", BackgroundTransparency = 0 },
+  Button = { Image = "rbxassetid://110958770625024", BackgroundTransparency = 0 },
   Corner = { CornerRadius = UDim.new(0, 6) }
 })
 
